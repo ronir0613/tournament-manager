0f2f1c85dedf1af262d73c346568eba5f593b239
ronir0613 <ronirapaka13@gmail.com>
2026-04-05 20:04:40 +0530

    a minor change in appjsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import TournamentDetail from "./pages/TournamentDetail";
 import Admin from "./pages/Admin";
+import { Navigate } from "react-router-dom";
+
 
 function App() {
-  const token = localStorage.getItem("token");
-  const role = localStorage.getItem("role");
-
+    const token = localStorage.getItem("token");
+    const isAuthenticated = !!token;
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* 🔥 PUBLIC ROUTES */}
-        <Route path="/" element={!token ? <Login /> : <Navigate to="/dashboard" />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* 🔥 PROTECTED ROUTES */}
-        <Route
-          path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/" />}
-        />
-
-        <Route
-          path="/tournament/:id"
-          element={token ? <TournamentDetail /> : <Navigate to="/" />}
-        />
-
-        {/* 🔥 ADMIN ONLY */}
-        <Route
-          path="/admin"
-          element={
-            token && role === "ADMIN" ? <Admin /> : <Navigate to="/dashboard" />
-          }
-        />
-
+        <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="/tournament/:id" element={<TournamentDetail />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
